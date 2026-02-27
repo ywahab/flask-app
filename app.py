9accdb3e8d4bdd40cdca2ea45843403b73810b7c
ywahab <78213529+ywahab@users.noreply.github.com>
2026-02-27 19:14:27 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+# app.py
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to the Flask API!"})
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
